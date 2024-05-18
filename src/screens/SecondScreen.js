00e552c389a09edbc19f2edee40433617ddf288f
Your Name <you@example.com>
2024-05-18 22:53:43 +0300

    newWhat

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -3,6 +3,7 @@ import Box from "../components/box/Box"
 import nomorology from "../images/שובל דעבול מסלולים 7.png"
 import tarot from "../images/שובל דעבול מסלולים 5.png"
 import teta from "../images/שובל דעבול מסלולים 6.png"
+import tetaHiling from "../images/תטא הילינג.png"
 import What from "../components/What/What"
 import clinic from "../images/שובל דעבול מסלולים 2.png"
 import clinic2 from "../images/שובל דעבול מסלולים 3.png"
@@ -49,18 +50,26 @@ const SecondScreen=()=>{
         itemType="https://schema.org/Service"
       />
       <What
-        title="טיפול תטא הילינג"
+        title="טיפול  משולב - טארוט + תטא הילינג"
         description="טיפול משולב - טארוט + תטא הילינג - לאחר סשן עוצמתי ומדוייק נסיים בהכוונה באמצעות קלפי הטארוט - מענה על 4 שאלות"
         src={clinic3}
-        message="היי שובל, אשמח לשמוע עוד על טיפול תטא הילינג"
+        message="היי שובל, אשמח לשמוע עוד על טיפול משולב של טארוט + תטא הילינג"
         itemScope
         itemType="https://schema.org/Service"
       />
-      <What
+          <What
         title="טיפול משולב נומורולוגיה + תטא הילינג"
         description="לפני הגעתך לטיפול אני חוקרת אודות המפה הנומורולוגית שלך + מעבר יחד איתך על המפה שלך + סשן טיפול בתת המודע"
         src={teta}
         message="היי שובל, אשמח לשמוע עוד על טיפול משולב נומורולוגיה + תטא הילינג"
+        itemScope
+        itemType="https://schema.org/Service"
+      />
+      <What
+        title="טיפול תטא הילינג"
+        description="שיח רגשי טיפולי המשלב ריפוי אנרגטי"
+        src={tetaHiling}
+        message="היי שובל, אשמח לשמוע עוד על טיפול תטא הילינג"
         itemScope
         itemType="https://schema.org/Service"
       />
